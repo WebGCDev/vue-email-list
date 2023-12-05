912e7a0b5730b91367bcab27a8628d6bd1d0b87d
WebGCDev <gaetano.chi@gmail.com>
2023-12-05 19:58:53 +0100

    feat: esercizio Completato

--- js/script.js
+++ js/script.js
@@ -2,6 +2,29 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      title: 'Lista di 10 email generate in maniera randomica:',
+      generatedEmail: [],
+    };
+  },
+  methods: {
+    getApi() {
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((myResponse) => {
+          this.generatedEmail.push(myResponse.data.response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    generateEmail(n) {
+      for (let i = 0; i < n; i++) {
+        this.getApi();
+      }
+    },
+  },
+  mounted() {
+    this.generateEmail(10);
   },
 }).mount('#app');
